5ca5e9ab540d5e945ce7737934188c98a9ddff1c
turboman <SXXT2015@outlook.com>
2024-08-07 07:45:04 +0800

    update code.

--- CodeInterviews/src/Tree/Traverse/treeTraverse.java
+++ CodeInterviews/src/Tree/Traverse/treeTraverse.java
@@ -1,25 +1,55 @@
 package CodeInterviews.src.Tree.Traverse;
 
+import java.util.Stack;
+
 public class treeTraverse {
     public static void main(String [] args){
-        TreeNode root = new TreeNode(1);
-
-
-    }
-
-    public void preOrderTraverse(TreeNode root){
-        if (root == null) return;
-         System.out.println(root.value);
-         preOrderTraverse(root.leftChild);
-         preOrderTraverse(root.rightChild);
+        int [] arr = new int [] {1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9};
+        TreeNode root = buildTree(arr, 0);
+        preOrderTraverse(root);
+        System.out.println("================");
+        midOrderTraverse(root);
+        System.out.println("================");
 
     }
 
-    public void midOrderTraverse(TreeNode root){
-        if (root == null) return;
-        preOrderTraverse(root.leftChild);
-        System.out.println(root.value);
-        preOrderTraverse(root.rightChild);
+    public static TreeNode buildTree(int [] array, int index){
+        if (index >= array.length || array[index] == -1 ) return null;
+        TreeNode root = new TreeNode(array[index]);
+        root.leftChild = buildTree(array, 2 * index + 1);
+        root.rightChild = buildTree(array, 2 * (index + 1));
+        return root;
+    }
+
+    public static void preOrderTraverse(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode curNode = stack.pop();
+            System.out.println(curNode.value);
+            if (curNode.rightChild != null)
+                stack.push(curNode.rightChild);
+            if (curNode.leftChild != null)
+                stack.push(curNode.leftChild);
+        }
+
+    }
+
+    public static void midOrderTraverse(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()){
+
+            while (root != null ){
+                stack.push(root);
+                root = root.leftChild;
+            }
+
+            root = stack.pop();
+            System.out.println(root.value);
+            root = root.rightChild;
+
+        }
 
     }
 
--- CodeInterviews/src/Tree/Traverse/treeTraverseRecursion.java
+++ CodeInterviews/src/Tree/Traverse/treeTraverseRecursion.java
@@ -2,11 +2,25 @@ package CodeInterviews.src.Tree.Traverse;
 
 public class treeTraverseRecursion {
     public static void main(String [] args){
-        TreeNode root = new TreeNode(1);
+        int [] arr = new int [] {1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9};
+        TreeNode root = buildTree(arr, 0);
+        preOrderTraverse(root);
+        System.out.println("================");
+        midOrderTraverse(root);
+        System.out.println("================");
+        postOrderTraverse(root);
 
     }
 
-    public void preOrderTraverse(TreeNode root){
+    public static TreeNode buildTree(int [] array, int index){
+        if (index >= array.length || array[index] == -1 ) return null;
+        TreeNode root = new TreeNode(array[index]);
+        root.leftChild = buildTree(array, 2 * index + 1);
+        root.rightChild = buildTree(array, 2 * (index + 1));
+        return root;
+    }
+
+    public static void preOrderTraverse(TreeNode root){
         if (root == null) return;
          System.out.println(root.value);
          preOrderTraverse(root.leftChild);
@@ -14,7 +28,7 @@ public class treeTraverseRecursion {
 
     }
 
-    public void midOrderTraverse(TreeNode root){
+    public static void midOrderTraverse(TreeNode root){
         if (root == null) return;
         preOrderTraverse(root.leftChild);
         System.out.println(root.value);
@@ -22,7 +36,7 @@ public class treeTraverseRecursion {
 
     }
 
-    public void postOrderTraverse(TreeNode root){
+    public static void postOrderTraverse(TreeNode root){
         if (root == null) return;
         preOrderTraverse(root.leftChild);
         preOrderTraverse(root.rightChild);
