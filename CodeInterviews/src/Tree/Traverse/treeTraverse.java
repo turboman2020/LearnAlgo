09db96d1187c785b2ab3e3852d12921956bb7e06
turboman <SXXT2015@outlook.com>
2024-08-09 07:49:11 +0800

    update code.

--- CodeInterviews/src/Tree/Traverse/treeTraverse.java
+++ CodeInterviews/src/Tree/Traverse/treeTraverse.java
@@ -1,15 +1,21 @@
 package CodeInterviews.src.Tree.Traverse;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
 public class treeTraverse {
     public static void main(String [] args){
         int [] arr = new int [] {1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9};
         TreeNode root = buildTree(arr, 0);
-        preOrderTraverse(root);
-        System.out.println("================");
-        midOrderTraverse(root);
-        System.out.println("================");
+        List<Integer> res ;
+        res = preOrderTraverse(root);
+        System.out.println(Arrays.toString(res.toArray()));
+        res = preOrderTraverse2(root);
+        System.out.println(Arrays.toString(res.toArray()));
+//        res = midOrderTraverse(root);
+//        System.out.println(Arrays.toString(res.toArray()));
 
     }
 
@@ -21,23 +27,43 @@ public class treeTraverse {
         return root;
     }
 
-    public static void preOrderTraverse(TreeNode root){
+    public static List<Integer> preOrderTraverse(TreeNode root){
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
+        List<Integer> res = new LinkedList<>();
+        if(root == null) return res;
         while (!stack.isEmpty()){
             TreeNode curNode = stack.pop();
-            System.out.println(curNode.value);
+            res.add(curNode.value);
             if (curNode.rightChild != null)
                 stack.push(curNode.rightChild);
             if (curNode.leftChild != null)
                 stack.push(curNode.leftChild);
         }
+        return res;
 
     }
 
-    public static void midOrderTraverse(TreeNode root){
+    public static List<Integer> preOrderTraverse2(TreeNode root){
         Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new LinkedList<>();
+        while (root!=null || !stack.isEmpty()){
+            while(root != null) {
+                res.add(root.value);
+                stack.push(root);
+                root = root.leftChild;
+            }
+            root = stack.pop();
+            root = root.rightChild;
+        }
 
+        return res;
+
+    }
+
+    public static List<Integer> midOrderTraverse(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new LinkedList<>();
         while (root != null || !stack.isEmpty()){
 
             while (root != null ){
@@ -46,10 +72,11 @@ public class treeTraverse {
             }
 
             root = stack.pop();
-            System.out.println(root.value);
+            res.add(root.value);
             root = root.rightChild;
 
         }
+        return res;
 
     }
 
