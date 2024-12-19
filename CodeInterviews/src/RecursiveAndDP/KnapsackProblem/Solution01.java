471a5a6271364ec03ec5bbb1be6710d3b4fe928a
turboman <SXXT2015@outlook.com>
2024-12-19 08:37:50 +0800

    完成01背包回溯法

--- CodeInterviews/src/RecursiveAndDP/KnapsackProblem/Solution01.java
+++ CodeInterviews/src/RecursiveAndDP/KnapsackProblem/Solution01.java
@@ -1,26 +1,48 @@
 package CodeInterviews.src.RecursiveAndDP.KnapsackProblem;
 
 public class Solution01 {
+    static int res = 0;
     public static void main(String [] array){
-            int [] weight = new int [] {1,2,3};
-            int [] value = new int [] {6,10,12};
-            int c = 5;
-            int maxValue = knapsackProblem(weight, value, c);
-            System.out.println(maxValue);
+
+        int [] weight = new int [] {6,5,6,6,3,7};
+        int [] value = new int [] {5,6,4,6,5,2};
+        int c = 15;
+        int maxValue = knapsackProblem(weight, value, c);
+        System.out.println(maxValue);
     }
 
     public static int knapsackProblem(int [] weight, int [] value, int c){
-        return knapsackProblemHelper(weight, value, c, weight.length-1);
+        // 方法一
+       return knapsackProblemHelper(weight, value, c, weight.length -1);
+       //方法二
+//        knapsackProblemHelper2(weight, value, c, 0, 0);
+//        return res;
     }
 
+    //递归法
     public static int knapsackProblemHelper(int [] weight, int [] value, int c, int index){
         if(c <= 0 || index < 0)
             return 0;
-        int maxValue = knapsackProblemHelper(weight, value, c-weight[index], index-1);
+        int maxValue = knapsackProblemHelper(weight, value, c, index-1);
 
         if( c >= weight[index]){
             maxValue = Math.max(maxValue, value[index] + knapsackProblemHelper(weight, value, c-weight[index], index-1));
         }
         return maxValue;
+    }
+
+    //回溯法
+    public static int knapsackProblemHelper2(int [] weight, int [] value, int c, int index, int curRes){
+        if(c <= 0   || index  >= weight.length || c < weight[index]) {
+            res = curRes > res ? curRes : res;
+            return res;
+        }
+        for(int i = index; i < weight.length; i++){
+            curRes += value[i];
+            knapsackProblemHelper2(weight, value, c- weight[i], i + 1, curRes);
+            curRes -= value[i];
+        }
+
+        return res;
     }
 }
