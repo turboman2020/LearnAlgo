acf2080947e01a2c514bf6c86508845ab7072689
turboman <SXXT2015@outlook.com>
2024-12-21 07:19:50 +0800

    完成01 DP的两种空间优化方法

--- CodeInterviews/src/RecursiveAndDP/KnapsackProblem/Solution02.java
+++ CodeInterviews/src/RecursiveAndDP/KnapsackProblem/Solution02.java
@@ -13,7 +13,7 @@ public class Solution02 {
 //        int [] weight = new int [] {1,2,3};
 //        int [] value = new int [] {6,10,12};
 //        int c = 5;
-        int maxValue = knapsackProblem(weight, value, c);
+        int maxValue = knapsackProblem04(weight, value, c);
         System.out.println(maxValue);
     }
 
@@ -38,10 +38,67 @@ public class Solution02 {
                 }
 
             }
-            System.out.println(Arrays.toString(memo[i]));
+            System.out.println(Arrays.toString(memo[i%2]));
 
         }
         return memo[weight.length-1][c];
+
+    }
+
+    public static int knapsackProblem02(int [] weight, int [] value, int c){
+        if(weight == null || weight.length == 0) return 0;
+        // DP方法， memo[i][j]表示遍历到第i个物品容量为j时的最大价值
+        memo = new int[weight.length][c+1];
+
+        for(int j = weight[0]; j <= c; j++){
+            memo[0][j] = value[0];
+        }
+
+        for(int i = 1 ;i < weight.length; i++){
+            // 可以从 j = weight[i]开始遍历
+            for(int j = weight[i]; j <= c; j++){
+                memo[i][j] = Math.max(memo[i-1][j], value[i] + memo[i-1][j-weight[i]]);
+            }
+            System.out.println(Arrays.toString(memo[i]));
+        }
+        return memo[weight.length-1][c];
+
+    }
+
+    public static int knapsackProblem03(int [] weight, int [] value, int c){
+        if(weight == null || weight.length == 0) return 0;
+        // DP方法，空间优化, 从行上来看，每次循环只与第（i-1） 和 i 有关
+        memo = new int[2][c+1];
+        for(int j = weight[0]; j <= c; j++){
+            memo[0][j] = value[0];
+        }
+
+        for(int i = 1 ;i < weight.length; i++){
+            // 可以从 j = weight[i]开始遍历
+            for(int j = weight[i]; j <= c; j++){
+                memo[i%2][j] = Math.max(memo[(i-1)%2][j], value[i] + memo[(i-1)%2][j-weight[i]]);
+            }
+            System.out.println(Arrays.toString(memo[i%2]));
+        }
+        return memo[(weight.length-1)%2][c];
+
+    }
+
+    public static int knapsackProblem04(int [] weight, int [] value, int c){
+        if(weight == null || weight.length == 0) return 0;
+        // DP方法，空间优化：
+        // 从行上来看，每次循环只与第（i-1） 和 i 有关；
+        // 从列上来看，后面列的计算会用到前面的列，因此内层循环j可以从c开始递减至weight[i],这样就只需要一个一维数组
+        int [] memo = new int[c+1];
+
+        for(int i = 0 ;i < weight.length; i++){
+            // 可以从 j = weight[i]开始遍历
+            for(int j = c; j >= weight[i]; j--){
+                memo[j] = Math.max(memo[j], value[i] + memo[j-weight[i]]);
+            }
+            System.out.println(Arrays.toString(memo));
+        }
+        return memo[c];
 
     }
 }
