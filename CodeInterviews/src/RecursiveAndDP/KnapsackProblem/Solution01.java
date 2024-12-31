dda424dddac058c7dbf2fccb9d3561f972490c3d
turboman <SXXT2015@outlook.com>
2024-12-31 18:26:43 +0800

    修改文件格式

--- CodeInterviews/src/RecursiveAndDP/KnapsackProblem/Solution01.java
+++ CodeInterviews/src/RecursiveAndDP/KnapsackProblem/Solution01.java
@@ -14,54 +14,51 @@ public class Solution01 {
 
     public static int knapsackProblem(int [] weight, int [] value, int c){
         // 方法一
-//        return knapsackProblemHelper(weight, value, c, weight.length -1);
+//        knapsackProblemHelper1(weight, value, c, 0, 0);
+//        return res;
 
         //方法二
-        //    knapsackProblemHelper2(weight, value, c, 0, 0);
-        //    return res;
+//        return knapsackProblemHelper2(weight, value, c, weight.length -1);
+
 
         // 方法三
         memo = new int[weight.length][c+1];
         for(int i = 0 ;i < weight.length; i++){
             for(int j = c; j >= 0; j--){
                     memo[i][j] = -1;
-
             }
-
         }
-
         return knapsackProblemHelper3(weight, value, c, weight.length -1);
 
     }
 
+    //回溯法 res为全局变量
+    public static void knapsackProblemHelper1(int [] weight, int [] value, int c, int index, int curRes){
+        if(c <= 0   || index  >= weight.length || c < weight[index]) {
+            res = curRes > res ? curRes : res;
+            return ;
+        }
+        for(int i = index; i < weight.length; i++){
+            curRes += value[i];
+            knapsackProblemHelper1(weight, value, c- weight[i], i + 1, curRes);
+            curRes -= value[i];
+        }
+    }
+
     //递归法
-    public static int knapsackProblemHelper(int [] weight, int [] value, int c, int index){
+    public static int knapsackProblemHelper2(int [] weight, int [] value, int c, int index){
         if(c <= 0 || index < 0)
             return 0;
-        int maxValue = knapsackProblemHelper(weight, value, c, index-1);
+        int maxValue = knapsackProblemHelper2(weight, value, c, index-1);
 
         if( c >= weight[index]){
-            maxValue = Math.max(maxValue, value[index] + knapsackProblemHelper(weight, value, c-weight[index], index-1));
+            maxValue = Math.max(maxValue, value[index] + knapsackProblemHelper2(weight, value, c-weight[index], index-1));
         }
         return maxValue;
     }
 
-    //回溯法
-    public static int knapsackProblemHelper2(int [] weight, int [] value, int c, int index, int curRes){
-        if(c <= 0   || index  >= weight.length || c < weight[index]) {
-            res = curRes > res ? curRes : res;
-            return res;
-        }
-        for(int i = index; i < weight.length; i++){
-            curRes += value[i];
-            knapsackProblemHelper2(weight, value, c- weight[i], i + 1, curRes);
-            curRes -= value[i];
-        }
 
-        return res;
-    }
-
-    //记忆化搜索
+    //记忆化搜索， memo 在全局定义，在调用函数中初始化
     public static int knapsackProblemHelper3(int [] weight, int [] value, int c, int index){
         if(c <= 0 || index < 0) {
             return 0;
@@ -82,4 +79,5 @@ public class Solution01 {
 
 
     }
+
 }
